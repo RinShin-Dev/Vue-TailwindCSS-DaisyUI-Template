2589ecc60235be9a0acb4e9ac07a2c9f9a962ad2
6MA-606 <zyxma.6666@hotmail.com>
2024-07-23 22:32:21 +0700

    1.0.0

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <main class="w-screen h-screen grid place-content-center gap-4">
+    <div class="text-2xl">Template by <a href="https://github.com/6MA-606" class="btn-link">6MA-606</a></div>
+    <div>
+      <div class="font-bold">This template contains</div>
+      <ul class="list-disc list-inside">
+        <li>Vue Router</li>
+        <li>Pinia State Management</li>
+        <li><a href="https://tailwindcss.com/" class="btn-link">TailwindCSS</a></li>
+        <li><a href="https://daisyui.com/" class="btn-link">DaisyUI</a></li>
+      </ul>
+    </div>
+  </main>
+</template>
